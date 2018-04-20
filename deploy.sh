9a2fb59bb92240083a0f120aa8faa5570fbc04e7
Stuart Craigen <s.craigen@mac.com>
2018-04-20 10:18:36 +0100

    Updated rds secret references

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,47 @@
-#!/usr/bin/env bash
-export KUBE_NAMESPACE=${KUBE_NAMESPACE:-${DRONE_DEPLOY_TO}}
+#!/bin/bash
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
 fi
 
+if [[ ${ENVIRONMENT} == "pr" ]] ; then
+    echo "deploy ${VERSION} to pr namespace, using PTTG_IP_PR drone secret"
+    export KUBE_TOKEN=${PTTG_IP_PR}
+else
+    if [[ ${ENVIRONMENT} == "test" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using PTTG_IP_TEST drone secret"
+        export KUBE_TOKEN=${PTTG_IP_TEST}
+    else
+        echo "deploy ${VERSION} to dev namespace, using PTTG_IP_DEV drone secret"
+        export KUBE_TOKEN=${PTTG_IP_DEV}
+    fi
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+fi
+
+export WHITELIST=${WHITELIST:-0.0.0.0/0}
+
+if [ "${ENVIRONMENT}" == "pr" ] ; then
+    export DNS_PREFIX=
+    export KC_REALM=pttg-production
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+    export KC_REALM=pttg-qa
+fi
+
+export DOMAIN_NAME=fs.${DNS_PREFIX}pttg.homeoffice.gov.uk
+
+echo "DOMAIN_NAME is $DOMAIN_NAME"
+
 cd kd
+
 kd --insecure-skip-tls-verify \
-   --file deployment.yaml \
-   --file service.yaml \
-   --retries 50
+    -f networkPolicy.yaml \
+    -f deployment.yaml \
+    -f service.yaml

f119601b30c1f1f6fbf5fed705198e3784c720a7
Mike Halma <mike.halma@digital.homeoffice.gov.uk>
2019-09-25 13:22:20 +0100

    EE-12559 Start RDS every morning on dev/est namespaces

--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,21 @@ if [[ ${ENVIRONMENT} == "pr" ]] ; then
     export KUBE_TOKEN=${PTTG_IP_PR}
     export DNS_PREFIX=
     export KC_REALM=pttg-production
-    export STOP_RDS_EVERY_NIGHT=false
 else
     export KUBE_TOKEN=${PTTG_IP_DEV}
     export DNS_PREFIX=${ENVIRONMENT}.notprod.
     export KC_REALM=pttg-qa
+fi
+
+# Production RDS is up constantly
+# dev/test RDS stops every night and starts every morning
+# feat/preprod RDS stops every night and is started on demand (by deploying pttg-postgres or scaling up from zero)
+export START_RDS_EVERY_MORNING=false
+export STOP_RDS_EVERY_NIGHT=false
+if [[ ${ENVIRONMENT} == "dev" ]] || [[ ${ENVIRONMENT} == "test" ]] ; then
+    export START_RDS_EVERY_MORNING=true
+fi
+if [[ ${ENVIRONMENT} != "pr" ]] ; then
     export STOP_RDS_EVERY_NIGHT=true
 fi
 
@@ -32,5 +42,6 @@ kd --insecure-skip-tls-verify \
     -f networkPolicy.yaml \
     -f rds-configmap.yaml\
     -f service.yaml \
+    -f start-rds-cronjob.yaml \
     -f stop-rds-cronjob.yaml \
     -f deployment.yaml
